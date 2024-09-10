17df0a7cc94784281e46cf6f22a2153bb458e66d
Дрійко КваКва <kupchak.a.m@gmail.com>
2024-09-10 17:58:43 +0300

    added LoggerMiddleware

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,14 +1,26 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CommandModule } from 'nestjs-command';
 import { SeedModule } from './seeds/seed.module';
 import { UserModule } from './users/users.module';
 import { PollsModule } from './polls/polls.module';
+import { LoggerMiddleware } from './middleware/logger.middleware';
 
 @Module({
   imports: [CommandModule, SeedModule, UserModule, PollsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes({ path: '*', method: RequestMethod.ALL });
+  }
+}
