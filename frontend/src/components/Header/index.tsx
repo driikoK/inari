c0cfef5b20f97cc68eca545ac9d3bb92f073d059
Jebbaited <kostadrapak@gmail.com>
2024-09-11 13:34:36 +0300

    fix header

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,27 +1,22 @@
-import { FunctionComponent, useState } from 'react';
-import {
-  HeaderContainer,
-  Logo,
-  LogoWrapper,
-  NavButton,
-  NavWrapper,
-  Paragraph,
-} from './styles';
+import { FunctionComponent, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { IconButton, Menu, MenuItem, useMediaQuery } from '@mui/material';
-import theme from '@theme';
 import MenuIcon from '@mui/icons-material/Menu';
+import { IconButton, Menu, MenuItem, useMediaQuery } from '@mui/material';
 
+import { HeaderContainer, Logo, LogoWrapper, NavButton, NavWrapper, Paragraph } from './styles';
+import theme from '@theme';
 const Header: FunctionComponent = () => {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
+  const [open, setOpen] = useState<boolean>(false);
 
   const handleClickMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
+    setOpen(true);
   };
 
   const handleCloseMenu = () => {
     setAnchorEl(null);
+    setOpen(false);
   };
 
   const navigate = useNavigate();
@@ -32,6 +27,10 @@ const Header: FunctionComponent = () => {
   };
 
   const isDesktop = useMediaQuery(theme.screens.desktop);
+
+  useEffect(() => {
+    handleCloseMenu();
+  }, [isDesktop]);
 
   return (
     <HeaderContainer>
@@ -45,27 +44,17 @@ const Header: FunctionComponent = () => {
           <NavButton onClick={() => handleLink('/vote')}>Голосування</NavButton>
         </NavWrapper>
       ) : (
-        <IconButton onClick={handleClickMenu}>
-          <MenuIcon color="secondary" />
-        </IconButton>
-      )}
+        <>
+          <IconButton onClick={handleClickMenu}>
+            <MenuIcon color="secondary" />
+          </IconButton>
 
-      <Menu
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleCloseMenu}
-        anchorOrigin={{
-          vertical: 'top',
-          horizontal: 'left',
-        }}
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'left',
-        }}
-      >
-        <MenuItem onClick={() => handleLink('/cookie')}>Крихти</MenuItem>
-        <MenuItem onClick={() => handleLink('/vote')}>Голосування</MenuItem>
-      </Menu>
+          <Menu anchorEl={anchorEl} open={open} onClose={handleCloseMenu}>
+            <MenuItem onClick={() => handleLink('/cookie')}>Крихти</MenuItem>
+            <MenuItem onClick={() => handleLink('/vote')}>Голосування</MenuItem>
+          </Menu>
+        </>
+      )}
     </HeaderContainer>
   );
 };
