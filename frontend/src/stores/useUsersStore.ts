45a460843cbe531f745bd6c000d5d65fb416a9ba
Jebbaited <kostadrapak@gmail.com>
2024-09-20 15:23:58 +0300

    add filters

--- frontend/src/stores/useUsersStore.ts
+++ frontend/src/stores/useUsersStore.ts
@@ -1,23 +1,34 @@
-/* eslint-disable no-useless-catch */
 import { create } from 'zustand';
 import axios from 'axios';
 import { UserType } from '@/types';
+import { isObjEmpty } from '@/utils/utility.utils';
 
+interface FilterData {
+  id: string;
+  role: string;
+}
 interface IState {
   users: UserType[];
-  getUsers: () => Promise<void>;
-  addUser: (newUser: unknown) => Promise<void>;
+  usersDictionary: UserType[];
+  getUsers: (filters?: Partial<FilterData>) => Promise<void>;
+  addUser: (newUser: Omit<UserType, '_id'>) => Promise<void>;
 }
 
 const useUsersStore = create<IState>((set) => ({
   users: [],
+  usersDictionary: [],
   loading: false,
   error: null,
 
-  getUsers: async () => {
+  getUsers: async (filters) => {
     try {
-      const response = await axios.get(`${process.env.API_URL}/users`);
-      set({ users: response.data });
+      const response = await axios.get(`${process.env.API_URL}/users`, { params: filters });
+
+      if (filters && !isObjEmpty(filters)) {
+        return set({ users: response.data });
+      }
+
+      set({ users: response.data, usersDictionary: response.data });
     } catch (error) {
       throw error;
     }
@@ -25,10 +36,7 @@ const useUsersStore = create<IState>((set) => ({
 
   addUser: async (newUser) => {
     try {
-      const response = await axios.post(
-        `${process.env.API_URL}/users`,
-        newUser
-      );
+      const response = await axios.post(`${process.env.API_URL}/users`, newUser);
       set((state) => ({ users: [...state.users, response.data] }));
     } catch (error) {
       throw error;
