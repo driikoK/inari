1402d60529699a1cd2322424341e173c6f28d6d4
Jebbaited <kostadrapak@gmail.com>
2024-09-21 19:41:44 +0300

    add file for other constants

---
+++ frontend/src/App/dialogs/InputCookieDialog/const/options.ts
@@ -0,0 +1,46 @@
+import { ANIME_TYPE } from '../types';
+
+export const titleTypeOptions = [
+  {
+    label: 'Серіал',
+    value: ANIME_TYPE.SERIES,
+  },
+  {
+    label: 'Фільм',
+    value: ANIME_TYPE.FILM,
+  },
+  {
+    label: 'Короткометражка',
+    value: ANIME_TYPE.SHORT_FILM,
+  },
+];
+
+export const seasonOptions = [
+  {
+    label: 'Зима',
+    value: 'winter',
+  },
+  {
+    label: 'Весна',
+    value: 'spring',
+  },
+  {
+    label: 'Літо',
+    value: 'summer',
+  },
+  {
+    label: 'Осінь',
+    value: 'fall',
+  },
+];
+
+export const yearOptions = [
+  {
+    label: '2024',
+    value: '2024',
+  },
+  {
+    label: '2025',
+    value: '2025',
+  },
+];
