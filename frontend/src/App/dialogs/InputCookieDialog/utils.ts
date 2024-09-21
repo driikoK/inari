d1a24e013e072630defa32be91a36e571419fd57
Jebbaited <kostadrapak@gmail.com>
2024-09-21 19:43:28 +0300

    remove unused functions

--- frontend/src/App/dialogs/InputCookieDialog/utils.ts
+++ frontend/src/App/dialogs/InputCookieDialog/utils.ts
@@ -1,4 +1,3 @@
-import { Dispatch, SetStateAction } from 'react';
 import { ValuesType } from './types';
 
 export const totalMainCoins = (values: ValuesType) => {
@@ -15,20 +14,4 @@ export const totalKbs = (values: ValuesType) => {
     0
   );
   return sumOfKbs;
-};
-
-export const getPartialValue = (percent: number, totalValue: number) => {
-  return (percent / 100) * totalValue;
-};
-
-export const handleChangeCurrentEpisode = (
-  event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>,
-  setCurrentEpisode: Dispatch<SetStateAction<string>>
-) => {
-  const value = event.target.value.replace(',', '.').replace(/[^0-9.]/g, '');
-  setCurrentEpisode(value);
-};
-
-export const isEpisodeValid = (episode: string) => {
-  return !isNaN(Number(episode)) && episode.length > 0;
 };
