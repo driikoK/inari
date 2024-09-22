d564d9aa4459b7df05ae16dd3315b303e7f35952
Jebbaited <kostadrapak@gmail.com>
2024-09-22 15:56:42 +0300

    update utilities

--- frontend/src/utils/season.utils.ts
+++ frontend/src/utils/season.utils.ts
@@ -1,24 +1,14 @@
-const baseYear = 2024;
-
-export function reverseDetermineAnimeSeason(seasonCode: number): string {
-  const seasonNumber = (seasonCode - 1) % 4 + 1;
-  const seasonYear = Math.floor((seasonCode + baseYear) / 4)
-
-  const seasonName = determineSeasonName(seasonNumber);
-  return `${seasonName} ${seasonYear}`;
-}
-
-function determineSeasonName(seasonNumber: number): string {
-  switch (seasonNumber) {
-      case 1:
-          return "Весна";
-      case 2:
-          return "Літо";
-      case 3:
-          return "Осінь";
-      case 4:
-          return "Зима";
-      default:
-          return "";
+export const convertSeasonEngToUkr = (season: string): string => {
+  switch (season) {
+    case 'spring':
+      return 'Весна';
+    case 'summer':
+      return 'Літо';
+    case 'fall':
+      return 'Осінь';
+    case 'winter':
+      return 'Зима';
+    default:
+      return '';
   }
-}
+};
--- frontend/src/utils/utility.utils.ts
+++ frontend/src/utils/utility.utils.ts
@@ -1,1 +1,5 @@
 export const isObjEmpty = (obj: Object = {}) => Object.keys(obj).length === 0;
+
+export const isObjValuesExist = (obj: Object = {}) => {
+  return Object.values(obj).some((value) => value);
+};
