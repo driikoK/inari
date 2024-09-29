c48bed4723dd30794d397b86d82221e800876a7e
Jebbaited <kostadrapak@gmail.com>
2024-09-29 23:09:15 +0300

    refactor

--- frontend/src/stores/useCoinsStore.ts
+++ frontend/src/stores/useCoinsStore.ts
@@ -3,6 +3,22 @@ import axios from '@/api';
 
 import { CoinsType } from '@/types';
 
+const defaultCoinValues = {
+  coins: 0,
+  sub: 0,
+  editor: 0,
+  dub: {
+    double: 0,
+    multi: 0,
+  },
+  fixer: 0,
+  roleBreaker: 0,
+  sound: 0,
+  releaser: 0,
+  director: 0,
+  another: 0,
+};
+
 interface IState {
   coinsTypes: {
     film: CoinsType;
@@ -16,51 +32,15 @@ const useCoinsStore = create<IState>((set) => ({
   coinsTypes: {
     film: {
       type: 'film',
-      coins: 0,
-      sub: 0,
-      editor: 0,
-      dub: {
-        double: 0,
-        multi: 0,
-      },
-      fixer: 0,
-      roleBreaker: 0,
-      sound: 0,
-      releaser: 0,
-      director: 0,
-      another: 0,
+      ...defaultCoinValues,
     },
     series: {
       type: 'series',
-      coins: 0,
-      sub: 0,
-      editor: 0,
-      dub: {
-        double: 0,
-        multi: 0,
-      },
-      fixer: 0,
-      roleBreaker: 0,
-      sound: 0,
-      releaser: 0,
-      director: 0,
-      another: 0,
+      ...defaultCoinValues,
     },
     shortFilm: {
       type: 'shortFilm',
-      coins: 0,
-      sub: 0,
-      editor: 0,
-      dub: {
-        double: 0,
-        multi: 0,
-      },
-      fixer: 0,
-      roleBreaker: 0,
-      sound: 0,
-      releaser: 0,
-      director: 0,
-      another: 0,
+      ...defaultCoinValues,
     },
   },
   loading: false,
