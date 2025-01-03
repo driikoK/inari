b9848b6f44d541bba28a7c152fc46354bd1c67b8
Jebbaited <kostadrapak@gmail.com>
2025-01-03 12:52:15 +0200

    fix build

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -1,18 +1,15 @@
 import { lazy } from 'react';
 import { RouteObject, useRoutes } from 'react-router-dom';
-
 import { Layout } from '@/components';
 
-const lazyImport = (path: string) => lazy(() => import(`../App/pages/${path}.tsx`));
-
-const Vote = lazyImport('Vote');
-const Result = lazyImport('Result');
-const Cookie = lazyImport('Cookie');
-const Home = lazyImport('Home');
-const CookieRating = lazyImport('CookieRating');
-const CookieList = lazyImport('CookieList');
-const Login = lazyImport('Login');
-const Settings = lazyImport('Settings');
+const Vote = lazy(() => import('../App/pages/Vote'));
+const Result = lazy(() => import('../App/pages/Result'));
+const Cookie = lazy(() => import('../App/pages/Cookie'));
+const Home = lazy(() => import('../App/pages/Home'));
+const CookieRating = lazy(() => import('../App/pages/CookieRating'));
+const CookieList = lazy(() => import('../App/pages/CookieList'));
+const Login = lazy(() => import('../App/pages/Login'));
+const Settings = lazy(() => import('../App/pages/Settings'));
 
 const conditionalRoutes = (condition: boolean, route: RouteObject) => (condition ? [route] : []);
 
@@ -21,8 +18,14 @@ const routes: RouteObject[] = [
     path: '/',
     element: <Layout />,
     children: [
-      { path: 'home', element: <Home /> },
-      { path: 'login', element: <Login /> },
+      {
+        path: 'home',
+        element: <Home />,
+      },
+      {
+        path: 'login',
+        element: <Login />,
+      },
       ...conditionalRoutes(process.env.VITE_FEATURE_POLL === 'true', {
         path: 'vote',
         element: <Vote />,
@@ -31,15 +34,29 @@ const routes: RouteObject[] = [
         path: 'result',
         element: <Result />,
       }),
-      { path: 'cookie', element: <Cookie /> },
-      { path: 'cookie/rating', element: <CookieRating /> },
-      { path: 'cookie/list', element: <CookieList /> },
-      { path: 'settings', element: <Settings /> },
-      { path: '*', element: <Home /> },
+      {
+        path: 'cookie',
+        element: <Cookie />,
+      },
+      {
+        path: 'cookie/rating',
+        element: <CookieRating />,
+      },
+      {
+        path: 'cookie/list',
+        element: <CookieList />,
+      },
+      {
+        path: 'settings',
+        element: <Settings />,
+      },
+      {
+        path: '*',
+        element: <Home />,
+      },
     ],
   },
 ];
-
 export default function Router() {
   return useRoutes(routes);
 }
