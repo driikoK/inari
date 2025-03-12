a19dc49b809c76e23b13cfe8a913112f6d1ee0f2
Jebbaited <kostadrapak@gmail.com>
2025-03-12 11:08:19 +0200

    fix routes visibility for admin role

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -1,6 +1,8 @@
 import { lazy } from 'react';
 import { RouteObject, useRoutes } from 'react-router-dom';
 import { Layout } from '@/components';
+import { usePermissions } from '@/hooks';
+import { SUBJECTS } from '@/context/casl';
 
 const Vote = lazy(() => import('../pages/Vote'));
 const Result = lazy(() => import('../pages/Result'));
@@ -13,50 +15,61 @@ const Settings = lazy(() => import('../pages/Settings'));
 
 const conditionalRoutes = (condition: boolean, route: RouteObject) => (condition ? [route] : []);
 
-const routes: RouteObject[] = [
-  {
-    path: '/',
-    element: <Layout />,
-    children: [
-      {
-        path: 'home',
-        element: <Home />,
-      },
-      {
-        path: 'login',
-        element: <Login />,
-      },
-      ...conditionalRoutes(process.env.VITE_FEATURE_POLL === 'true', {
-        path: 'vote',
-        element: <Vote />,
-      }),
-      ...conditionalRoutes(process.env.VITE_FEATURE_RESULTS === 'true', {
-        path: 'result',
-        element: <Result />,
-      }),
-      {
-        path: 'cookie',
-        element: <Cookie />,
-      },
-      {
-        path: 'cookie/rating',
-        element: <CookieRating />,
-      },
-      {
-        path: 'cookie/list',
-        element: <CookieList />,
-      },
-      {
-        path: 'settings',
-        element: <Settings />,
-      },
-      {
-        path: '*',
-        element: <Home />,
-      },
-    ],
-  },
-];
+const useConditionalRoutes = () => {
+  const { hasAccess } = usePermissions();
+
+  const routes: RouteObject[] = [
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        {
+          path: 'home',
+          element: <Home />,
+        },
+        {
+          path: 'login',
+          element: <Login />,
+        },
+        ...conditionalRoutes(process.env.VITE_FEATURE_POLL === 'true', {
+          path: 'vote',
+          element: <Vote />,
+        }),
+        ...conditionalRoutes(
+          hasAccess(SUBJECTS.SHOW_RESULTS) || process.env.VITE_FEATURE_RESULTS === 'true',
+          {
+            path: 'result',
+            element: <Result />,
+          }
+        ),
+        {
+          path: 'cookie',
+          element: <Cookie />,
+        },
+        {
+          path: 'cookie/rating',
+          element: <CookieRating />,
+        },
+        {
+          path: 'cookie/list',
+          element: <CookieList />,
+        },
+        {
+          path: 'settings',
+          element: <Settings />,
+        },
+        {
+          path: '*',
+          element: <Home />,
+        },
+      ],
+    },
+  ];
+
+  return routes;
+};
+
 export default function Router() {
+  const routes = useConditionalRoutes();
   return useRoutes(routes);
 }
